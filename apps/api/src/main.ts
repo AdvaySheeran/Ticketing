60e4c326a20f66740cb01c62e8310d16f0f4275f
Sampath Mattapalli <sampathmattapalli@gmail.com>
2026-05-23 22:54:02 +0530

    feat: API corsconfigand prefix added

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
