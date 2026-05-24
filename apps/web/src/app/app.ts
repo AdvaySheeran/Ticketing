97a1c14135d5a61260d5d61bdfe08f0ff363bf66
Sampath Mattapalli <sampathmattapalli@gmail.com>
2026-05-24 13:42:09 +0530

    feat: Navbar created

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/services/auth.service';
+import { NavbarComponent } from './shared/components/navbar-component/navbar-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(public authService: AuthService) {}
 
   ngOnInit(): void {
     this.authService.initFromToken();
---
+++ apps/web/src/app/shared/components/navbar-component/navbar-component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar-component.html',
+  styleUrl: './navbar-component.scss',
+})
+export class NavbarComponent {
+  constructor(private router: Router, public authService: AuthService) {}
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
